0b450aff5c1dbfa5892574ca3fe870e647acb139
John Lium <John.Lium@aunalytics.com>
2013-08-20 11:03:43 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,54 @@
+## Example
+`
+var conf = {username: "USERNAME", 
+            privateKey: "PATH_TO_PRIVATE_KEY",
+            bash: ['uptime',
+                   'uname -a',
+                   'echo "It worked!"']}
+se = require("sshexe")
+se(conf, function(err) {
+  if (err) throw err;
+  console.log("Test success");
+})
+`
+
+## Config parameters
+
+### host
+Default -- `localhost`
+
+The hostname of the ssh server to execute commands against.
+
+
+### port
+Default -- `22`
+
+The port on the ssh server to connect to.
+
+
+### username
+Default -- `root`
+
+The username to authenticate as for the ssh session. This might be a bad 
+default.
+
+
+### privateKey
+Default -- `"$HOME/.ssh/id\_rsa"`
+
+The private key to use for authentication. Passed as a string, the full
+path to the key file. Need to provide and param to specify a password to
+unlock encrypeted keys.
+
+
+### env
+Default -- `{}`
+
+Environment variables to set for the ssh session. Keys are the variable
+names, values are the values. Set with an export statement in the beginning of the session. Currently not functioning.
+
+
+### bash 
+Default -- `[]`
+
+A string or array of strings to execute.
---
+++ sshexe.js
@@ -0,0 +1,101 @@
+#! /usr/bin/env node
+
+var Connection = require('ssh2');
+var async = require('async');
+
+module.exports = sshexe
+
+function sshexe(conf, cb) {
+  var self = {},
+      conn = self.connection = new Connection();
+
+  conf = conf || {};
+  conf.host = conf.host || 'localhost';
+  conf.port = conf.port || 22;
+  conf.username = conf.username || 'root';
+  conf.privateKey = require('fs').readFileSync(conf.privateKey || process.env.HOME + "/.ssh/id_rsa");
+  conf.env = conf.env || {};
+  conf.bash = conf.bash || [];
+
+  self.conf = conf
+
+  self.callCmd = function(item, next) {
+    next = next || function(err){}
+    conn.exec(item, function(err, stream) {
+      console.log('Calling ' + item)
+      if (err) throw err;
+
+      stream.on('data', function(data, extended) {
+        console.log((extended === 'stderr' ? 'STDERR: ' : 'STDOUT: ')
+                    + data);
+      });
+
+      stream.on('end', function() {
+        console.log('Stream :: EOF');
+      });
+
+      stream.on('close', function() {
+        console.log('Stream :: close');
+      });
+
+      stream.on('exit', function(code, signal) {
+        console.log('Stream :: exit :: code: ' + code + ', signal: ' + signal);
+        next()
+      });
+
+    });
+  }
+
+  self.call = function(calls, cb) {
+    if (Array.isArray(calls)) {
+      async.eachSeries(calls, self.callCmd, function(err) {
+        conn.end();
+        if (err) cb(err);
+      });
+    }
+    else {
+      self.callCmd(calls);
+    }
+  }
+
+  conn.on('connect', function() {
+    console.log('Connection :: connect');
+  });
+
+  conn.on('error', function(err) {
+    console.log('Connection :: error :: ' + err);
+    cb(err)
+  });
+
+  conn.on('end', function() {
+    console.log('Connection :: end');
+  });
+
+  conn.on('close', function(had_error) {
+    console.log('Connection :: close');
+    cb();
+  });
+
+  conn.on('ready', function() {
+    console.log('Connection :: ready');
+    self.call(conf.bash, cb);
+  });
+
+  conn.connect(conf);
+}
+
+function genExportStr(cmd) {
+  var conn = this;
+  var env_str = "export"
+  Object.keys(conn.env).forEach(function(key) {
+    var this_env = [key, conn.env[key]].join("=");
+    env_str = [env_str, this_env].join(" ");
+  });
+  if (env_str === "export") {
+    return ""
+  }
+  else {
+    return env_str
+  }
+}
+
